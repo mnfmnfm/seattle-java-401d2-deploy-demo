c076eca37d8049b5bce7a4ce6895599a19706f28
mnfmnfm <mnfmnfm@gmail.com>
2018-12-10 16:40:12 -0800

    finish path variables

--- src/main/java/com/ferreirae/demo/HelloWorldController.java
+++ src/main/java/com/ferreirae/demo/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.ferreirae.demo;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ public class HelloWorldController {
     @RequestMapping("/")
     public String helloWorld() {
         return "Hello world! This is another change!";
+    }
+
+    @RequestMapping("/hello/{pineapple}/{fromPerson}")
+    public String helloToYou(@PathVariable(name="pineapple") String myName, @PathVariable String fromPerson) {
+        return "Hello " + myName + ", from " + fromPerson + "!";
     }
 }
