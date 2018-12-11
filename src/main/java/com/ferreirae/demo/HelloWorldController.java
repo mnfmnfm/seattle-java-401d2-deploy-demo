7e30bc89ab25f1e4faca07a08aa48061be463145
mnfmnfm <mnfmnfm@gmail.com>
2018-12-11 15:33:09 -0800

    views basics are there

--- src/main/java/com/ferreirae/demo/HelloWorldController.java
+++ src/main/java/com/ferreirae/demo/HelloWorldController.java
@@ -7,14 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+// uses templating!
 @Controller
 public class HelloWorldController {
 
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
     @RequestMapping("/hello/{pineapple}/{fromPerson}")
     public String helloToYou(@PathVariable(name="pineapple") String myName,
                              @PathVariable String fromPerson,
                              Model model) {
         model.addAttribute("myName", myName);
-        return "hello";
+        return "potato";
     }
 }
