24aeea91d0cd198fa09ce4bcc2d75e37e55db68d
mnfmnfm <mnfmnfm@gmail.com>
2018-12-10 18:09:55 -0800

    it works now

--- src/main/java/com/ferreirae/demo/DemoApplication.java
+++ src/main/java/com/ferreirae/demo/DemoApplication.java
@@ -1,13 +1,17 @@
 package com.ferreirae.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class);
 	}
 }
--- src/main/java/com/ferreirae/demo/HelloWorldController.java
+++ src/main/java/com/ferreirae/demo/HelloWorldController.java
@@ -1,18 +1,20 @@
 package com.ferreirae.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWorldController {
-    @RequestMapping("/")
-    public String helloWorld() {
-        return "Hello world! This is another change!";
-    }
 
     @RequestMapping("/hello/{pineapple}/{fromPerson}")
-    public String helloToYou(@PathVariable(name="pineapple") String myName, @PathVariable String fromPerson) {
-        return "Hello " + myName + ", from " + fromPerson + "!";
+    public String helloToYou(@PathVariable(name="pineapple") String myName,
+                             @PathVariable String fromPerson,
+                             Model model) {
+        model.addAttribute("myName", myName);
+        return "hello";
     }
 }
